50614059852f2a387d151d38720ab870008669ee
kermit1013 <k84101311@gmail.com>
2022-05-19 06:52:37 -0400

    supported markdown format

--- src/Survey.js
+++ src/Survey.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as Survey from "survey-core";
 import * as SurveyReact from "survey-react-ui";
 import "survey-core/defaultV2.css";
+import showdown from "showdown";
 
 import { json } from "./survey_json.js";
 
@@ -17,6 +18,19 @@ function onComplete(result) {
 
 export function SurveyPage() {
     var model = new Survey.Model(json);
+ //Create showdown markdown converter
+var converter = new showdown.Converter();
+model
+    .onTextMarkdown
+    .add(function (survey, options) {
+        //convert the markdown text to html
+        var str = converter.makeHtml(options.text);
+        //remove root paragraphs <p></p>
+        str = str.substring(3);
+        str = str.substring(0, str.length - 4);
+        //set html
+        options.html = str;
+    });
     return (
         <div className="container">
             <SurveyReact.Survey
